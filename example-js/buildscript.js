ef4cdc72cf5cdde95fcaa48a4d2df1d851a7d791
Yoonchae Lee <bluegreenmagick@gmail.com>
2023-06-24 11:03:21 +0900

    In examples, add "svelte" to `conditions` in esbuild build options This is needed to import svelte component libraries

--- example-js/buildscript.js
+++ example-js/buildscript.js
@@ -12,6 +12,7 @@ esbuild
     .build({
         entryPoints: ["./entry.js"],
         mainFields: ["svelte", "browser", "module", "main"],
+        conditions: ["svelte"],
         outdir: "./dist",
         format: "esm",
         logLevel: "info",
