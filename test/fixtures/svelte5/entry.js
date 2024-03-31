426184082bbf9836776e650b9157e469726ea930
Ethan Hampton <etho@ethohampton.com>
2024-03-31 15:32:09 -0500

    Add basic svelte 5 features test

---
+++ test/fixtures/svelte5/entry.js
@@ -0,0 +1,4 @@
+import { mount } from 'svelte';
+import Test from './svelte5.svelte';
+
+const test = mount(Test, { target: document.body });
---
+++ test/fixtures/svelte5/svelte5.svelte
@@ -0,0 +1,14 @@
+<script>
+	let count = $state(0);
+	let double = $derived(count * 2);
+	$effect(() => {
+		if (count > 10) {
+			alert('Too high!');
+		}
+
+	});
+</script>
+
+<button onclick={() => count++}>
+	{count} / {double}
+</button>
