0720b1409ab8923ac7ec13f546f43523d0e897d5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-05-18 01:32:34 +0200

    Update examples to use Svelte 5

--- example-ts/entry.ts
+++ example-ts/entry.ts
@@ -1,5 +1,9 @@
 import Test from "./index.svelte";
 import { haha } from "./fun";
 
-new Test({ target: document.body });
+import { mount } from "svelte";
+
+mount(Test, {
+    target: document.body,
+});
 console.log(haha());
